e27d90098992f062efdef9a1bc3ff8dd667b2327
MPeets <mihkelpeets65@gmail.com>
2026-05-04 12:07:01 +0300

    infra: alarm on ALB target HTTP 5xx via ops SNS topic

--- infra/modules/alb/outputs.tf
+++ infra/modules/alb/outputs.tf
@@ -9,3 +9,13 @@ output "dns_name" {
 output "target_group_arn" {
   value = aws_lb_target_group.this.arn
 }
+
+output "load_balancer_arn_suffix" {
+  description = "Suffix for CloudWatch ApplicationELB LoadBalancer dimension."
+  value       = aws_lb.this.arn_suffix
+}
+
+output "target_group_arn_suffix" {
+  description = "Suffix for CloudWatch ApplicationELB TargetGroup dimension."
+  value       = aws_lb_target_group.this.arn_suffix
+}
